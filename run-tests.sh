03fb5187836c0500e599ac29daa0e2aabe5a6af4
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-09-22 13:53:13 +0000

    remove fluff from runtests

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,5 @@
 # oarepo-s3 is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle invenio_s3 tests docs && \
-isort -c -df oarepo_s3 && \
 check-manifest --ignore ".travis-*" && \
 pytest -s tests/
